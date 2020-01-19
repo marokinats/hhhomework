ea041982bb30df2c8957f1bf7f5f973781966947
Tania Marokina <tmarokina@synergy.ru>
2020-01-20 00:13:57 +0300

    homework marokina

--- core/calculator.py
+++ core/calculator.py
@@ -1,15 +1,45 @@
 from decorators import cache_decorator
 
-def calculator(a, b, operation):
-    # Здесь нужно реализовать функцию,
-    # которая реализует основные арифметические операции между числами: +, -, /, *, **.
-    # Так же следует сделать проверку, что поступивший оператор корректен (присутствует в этом списке +, -, /, *, **)
-    return a + b
+@cache_decorator
 
+def calculator(a, b, operation):
+  # Здесь нужно реализовать функцию,
+  # которая реализует основные арифметические операции между числами: +, -, /, *, **.
+  # Так же следует сделать проверку, что поступивший оператор корректен (присутствует в этом списке +, -, /, *, **)
+  operations = ('+', '-', '/', '*','**')
+
+  if any(i == operation for i in operations):
+    if (operation == '+'):
+      return a + b
+    elif (operation == '-'):
+      return a - b
+    elif (operation == '*'):
+      return a * b
+    elif (operation == '/'):
+      try:
+        return a / b
+      except ZeroDivisionError:
+        return print('делить на ноль нельзя!')
+    elif (operation == '**'):
+      return a ** b
+  else:
+    return 'Неверно указан оператор, укажите один из +, -, /, *, **'
 
 if __name__ == '__main__':
-    a = int(input('Введите число: ')) # Тут было бы неплохо обрабатывать ошибку в случае передачи некорректных символов
-    b = int(input('Введите число: '))
-    operation = input('Введите операцию')
+  while True:
+    try:
+      a = int(input('Введите число: ')) 
+      break
+    except Exception:
+      print('Введите целое число')
+  while True:
+    try:
+      b = int(input('Введите число: '))
+      break
+    except Exception:
+      print('Введите целое число')
 
-    print('Результат: ', calculator(a, b, operation))
+  operation = input('Введите операцию')
+
+  print('Результат: ', calculator(a, b, operation))
+
