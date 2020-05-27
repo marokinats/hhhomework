4b5098b3739a21b8302aac91d3d556e97bb46a1e
Tania Marokina <tania.marokina@gmail.com>
2020-05-27 21:41:56 +0300

    python tasks (#1)
    * homework marokina
    
    * правки по комментариям
    
    Co-authored-by: Tania Marokina <tmarokina@synergy.ru>

--- core/decorators.py
+++ core/decorators.py
@@ -1,5 +1,15 @@
-def cache_decorator():
-    # Реализовать декоратор который кэширует результаты вызова функции (есть в лекции)
-    # Применить для функции calculator (в calculator.py уже есть import функции cache_decorator)
-    # Настоятельно прошу написать декоратор руками, а не копировать, т.к. важно запомнить как это работает
-    pass
+def cache_decorator(f):
+  # Реализовать декоратор который кэширует результаты вызова функции (есть в лекции)
+  # Применить для функции calculator (в calculator.py уже есть import функции cache_decorator)
+  # Настоятельно прошу написать декоратор руками, а не копировать, т.к. важно запомнить как это работает
+    
+  cache ={}
+
+  def inner(*args):
+
+    if args not in cache:
+      cache[args] = f(*args)
+
+    return cache[args]
+    
+  return inner
--- core/generators.py
+++ core/generators.py
@@ -1,5 +1,10 @@
+import random
 # написать генераторную функцию, которая принимает число N возвращает N рандомных чисел от 1 до 100
 def gen(N):
-    pass
+
+    for i in range(N):
+     
+      yield random.randint(0, 100)
 
 # написать генераторное выражение, которое делает то же самое
+gen2 = (random.randint(0, 100) for i in range(10))
